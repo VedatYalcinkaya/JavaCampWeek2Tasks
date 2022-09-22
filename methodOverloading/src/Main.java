995eb6b5c5b191e488a16faadb89bd891092302a
vedat <vedatylcnky@gmail.com>
2022-09-22 20:23:35 +0300

    Java Camp Week 2 Tasks

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BasicArithmeticOperations basicArithmeticOperations = new BasicArithmeticOperations();
+        System.out.println(basicArithmeticOperations.addition(2, 3));
+        System.out.println(basicArithmeticOperations.addition(2, 3, 5));
+
+    }
+}
